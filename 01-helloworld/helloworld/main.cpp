31105d87d4f61396ecc1d5e3c7283235cc55776c
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-05-05 12:21:40 +0900

    rename folder.

---
+++ 01-helloworld/helloworld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Yoshihisa Kaino on 2020/05/05.
+//  Copyright © 2020 yoshi1125hisa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "hogehoge";
+    std::cout << "hello world! \n";
+    return 0;
+}
